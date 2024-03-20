81a60d5d697bf0d96460051c263dbdb521ee5608
Bence Szabo <szabobence021@gmail.com>
2024-03-20 15:04:07 +0100

    toggle-gomb hozzáadása a setTitle konténerbe, annak az eseménykezelője, és style-ja

--- app.js
+++ app.js
@@ -18,28 +18,45 @@ document.addEventListener("DOMContentLoaded", function () {
         data.cards.forEach(card => {
 
           if (setTitle !== card.SetNumber){
+
             setTitle = card.SetNumber;
+
+            //setTitle Container
             const setTitleContainer = document.createElement('div');
-            const Title = document.createTextNode(setTitle); 
             setTitleContainer.classList.add('set-title');
-            setTitleContainer.appendChild(Title);
             cardContainer.appendChild(setTitleContainer);
+
+            const Title = document.createElement('div');
+            Title.innerText = setTitle;
+            Title.classList.add('title-text');
+            setTitleContainer.appendChild(Title);
+
+            var toggleButton = document.createElement("button");
+            toggleButton.type = "button";
+            toggleButton.className = "btn btn-primary btn-toggle";
+            toggleButton.setAttribute("data-toggle", "button");
+            toggleButton.setAttribute("aria-pressed", "false");
+            toggleButton.setAttribute("autocomplete", "off");
+            toggleButton.innerText = "=>";
+            toggleButton.dataset.b_setNumber = setTitle;
+            toggleButton.classList.add('collapse-button');
+            setTitleContainer.appendChild(toggleButton);
+
           }
 
-
-          if (card.HasImage === 'YES') {
-
-            //belső container
+          //cards
+          
+          //belső container
             const innerContainer = document.createElement('div');
+            innerContainer.dataset.setNumber = setTitle;
             cardContainer.appendChild(innerContainer);
 
-            //belső-fejléc container
+          //belső-fejléc container
             const headerContainer = document.createElement('div');
             headerContainer.classList.add('header-container');
             innerContainer.appendChild(headerContainer);
 
-
-            //név
+          //név
             const cardName = document.createElement('div');
             const Name = document.createTextNode(card.CharacterName); 
             cardName.classList.add('name');
@@ -54,19 +71,16 @@ document.addEventListener("DOMContentLoaded", function () {
             cardName.addEventListener('mouseout', function() {
               cardName.style.overflow = 'hidden';
               cardName.style.whiteSpace = 'nowrap';
-            });
-            
+            });           
 
-
-            //ritkaság
+          //ritkaság
             const cardRarity = document.createElement('div');
             const Rarity = document.createTextNode(card.Rarity);
             cardRarity.classList.add('rarity');
             cardRarity.appendChild(Rarity);
-            headerContainer.appendChild(cardRarity);
-            
+            headerContainer.appendChild(cardRarity);   
 
-            //cím
+          //cím
             const cardSeries = document.createElement('div');
             const Series = document.createTextNode(card.SeriesName);
             cardSeries.classList.add('series-name');
@@ -83,8 +97,7 @@ document.addEventListener("DOMContentLoaded", function () {
               cardSeries.style.whiteSpace = 'nowrap';
             });
 
-
-            //kép
+          //kép
             const cardImage = document.createElement('img');       
             cardImage.referrerPolicy="noreferrer";
             cardImage.src = card.ImageUrl;
@@ -93,8 +106,27 @@ document.addEventListener("DOMContentLoaded", function () {
             innerContainer.appendChild(cardImage);
             
 
-          }
         });
+
+        // Eseménykezelő hozzáadása a gombhoz
+        const toggleButtons = document.querySelectorAll('.collapse-button');
+        toggleButtons.forEach(button => {
+            button.addEventListener("click", function () {
+                const setTitle = this.dataset.b_setNumber;
+                const innerContainers = document.querySelectorAll(`[data-set-number="${setTitle}"]`);
+                innerContainers.forEach(container => {
+                    if (container.dataset.setNumber === setTitle) {
+                        container.classList.toggle('collapsed');
+                        if (container.classList.contains('collapsed')) {
+                            container.style.height = '0'; // Összecsukott állapotban a magasság nullára állítása
+                        } else {
+                            container.style.height = 'auto'; // Kinyitott állapotban az eredeti magasság visszaállítása
+                        }
+                    }
+                });
+            });
+        });
+
       })
       .catch(error => console.error('Error loading JSON:', error));
   });
